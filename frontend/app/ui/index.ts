d371dc4515b4a0503cab62749c0e2ff85b487483
Vitus Idi <vitus.idi@yahoo.com>
2026-01-22 14:57:32 +0200

    Restore ui/index.ts from Cursor dump

---
+++ frontend/app/ui/index.ts
@@ -0,0 +1,12 @@
+export * from './icons/IconPack';
+export { default as IconBase } from './icons/IconBase';
+
+export { default as RukaButton } from './components/Button';
+export { default as RukaCard } from './components/Card';
+export { default as SearchBar } from './components/SearchBar';
+export { default as Flashcard } from './components/Flashcard';
+export { default as Toggle } from './components/Toggle';
+
+export { theme } from './themes/theme';
+export { colors as uiColors } from './themes/colors';
+export { shadows as uiShadows } from './themes/shadows';
