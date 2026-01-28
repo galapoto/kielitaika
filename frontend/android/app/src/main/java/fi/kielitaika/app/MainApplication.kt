84cd0f7a36f76c0120668c13e24b697c61e32a73
Vitus Idi <vitus.idi@yahoo.com>
2026-01-28 20:46:57 +0200

    fix(android): register gesture-handler package

--- frontend/android/app/src/main/java/fi/kielitaika/app/MainApplication.kt
+++ frontend/android/app/src/main/java/fi/kielitaika/app/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.ReactHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.swmansion.gesturehandler.RNGestureHandlerPackage
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -23,8 +24,8 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+                // Ensure gesture handler is registered explicitly.
+                add(RNGestureHandlerPackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
